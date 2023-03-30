03a5ecd118aa548e70a754f2f960cede5fbe962e
DariethS <129444744+DariethS@users.noreply.github.com>
2023-03-30 15:24:17 -0500

    Update articulod.h

--- articulod.h
+++ articulod.h
@@ -1,19 +1,16 @@
 #include <iostream>
 #include "articulo.h"
-#ifndef articuloS_H
-#define articuloS_H
+#ifndef articuloD_H
+#define articuloD_H
 
-class ArticuloC : public Articulo{
-public:
-  ArticuloC(string nombre, int id, int precio, int disponibilidad, string tipo, int impuesto) : Articulo(nombre, id, precio, disponibilidad, tipo, impuesto){}
-  int get_Precio() override{
-      return this->precio+this->impuesto;
-    }
-  void set_Impuesto(int impuesto){
-    this->impuesto=impuesto;
-  }
-  int get_Impuesto(){
-    return this->impuesto;
-  }
+using namespace std;
+
+class ArticuloD : public Articulo{
+  public:
+    //Método constructor
+    ArticuloD(string ,int ,int ,int, string);
 };
+//Inicializamos el Método constructor que llama al constructor de la clase padre
+ArticuloD::ArticuloD(string nombre,int id, int precio, int disponibilidad, string tipo) : Articulo(nombre,id,precio,disponibilidad, tipo){}
+
 #endif
