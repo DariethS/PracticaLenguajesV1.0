fd4c7a75f67ce762697510bd3c18416a06008e1b
DariethS <129444744+DariethS@users.noreply.github.com>
2023-03-30 15:29:07 -0500

    Create usuario.h

---
+++ usuario.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "lista.h"
+#include "nodo.h"
+#ifndef usuario_H
+#define usuario_H
+using namespace std;
+
+class Usuario{
+  public:
+  //Método constructor
+    Usuario(){}
+};
+#endif
