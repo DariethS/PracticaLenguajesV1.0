0aac2bc4625f8b1cc7eac77c67d64e26062f4261
DariethS <129444744+DariethS@users.noreply.github.com>
2023-03-30 15:27:30 -0500

    Create inventario.h

---
+++ inventario.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "lista.h"
+#ifndef inventario_h
+#define inventario_h
+
+using namespace std;
+class Inventario : public Lista{
+  public:
+    Inventario():Lista(){}
+};
+
+#endif
